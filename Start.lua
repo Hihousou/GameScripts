899eab4f9eda2d0e7ec892a0f28a46a48c49e19c
Hihousou <108065780+Hihousou@users.noreply.github.com>
2022-07-18 11:44:56 +1000

    Update Start.lua

--- Start.lua
+++ Start.lua
@@ -1,9 +1,21 @@
+-- True = Hidden
+-- False = Shown
+
 local Games = {
-	[142823291] = "mm2",
-    [8783157237] = "8783157237", -- game name is "有感筆電⚔️槍戰", which is problematic
+	[142823291] = {
+		Name = "mm2",
+		Hidden = true}
+	,
+	[8783157237] = {
+		Name = "8783157237", -- game name is "有感筆電⚔️槍戰", which is problematic
+		Hidden = false
+	},
 }
 
-local GameName = Games[game.PlaceId]
-if GameName then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Hihousou/GameScripts/main/" .. GameName .. ".lua"))()
+local GameData = Games[game.PlaceId]
+if GameData then
+	task.spawn(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Hihousou/GameScripts/main/" .. GameData[Name] .. ".lua"))()
+	end)
+	return GameData[Hidden] or false
 end
