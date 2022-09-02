80c38cc297ff2d82c10cf38c59b5ef882623f9d4
Hihousou <108065780+Hihousou@users.noreply.github.com>
2022-09-02 22:00:38 +1000

    Update Start.lua

--- Start.lua
+++ Start.lua
@@ -3,6 +3,10 @@ local Games = {
 		Name = "mm2",
 		Hidden = true,
 	},
+	[606849621] = {
+		Name = "jailbreak",
+		Hidden = false,
+	},
 	[8783157237] = {
 		Name = "8783157237", -- game name is "有感筆電⚔️槍戰", which is problematic
 		Hidden = false,
