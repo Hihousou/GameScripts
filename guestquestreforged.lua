218d78d40e6d4b4a2e1c341ab5166a1c47808b50
Hihousou <108065780+Hihousou@users.noreply.github.com>
2022-09-16 20:45:14 +1000

    Update guestquestreforged.lua

--- guestquestreforged.lua
+++ guestquestreforged.lua
@@ -12,11 +12,40 @@ function get()
     end
 end
 
+
+local Connection = nil
+function SetupTool(Tool)
+	Connection = Tool.DescendantAdded:Connect(function(Child)
+		if Child:IsA("TouchTransmitter")then
+			for _,v in pairs(workspace.Mobs.Alive:GetChildren())do
+				if v and v.PrimaryPart then
+					firetouchinterest(Child.Parent,v.PrimaryPart,0)
+				end
+			end
+			
+			Connection:Disconnect()
+		end
+	end)
+	
+	Tool.RemoteEvent:FireServer(lPlayer.Character,lPlayer.Character.Humanoid,lPlayer.Character.Torso)
+end
+function HitAll()
+	if lPlayer.Character then
+		local Tool = lPlayer.Character:FindFirstChildOfClass("Tool")
+		if Tool then
+			SetupTool(Tool)
+		end
+	end
+end
+HitAll()
+
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hihousou/Hexploit/main/uilib.lua"))()
-local Window = Library:Window("Guest Quest Reforged")
+local Window = Library:Window("GQ: Reforged")
 Window:Toggle("Auto Attack (Bow)",false,function(Bool)
 	AutoAttack = Bool
 end)
+Window:Button("Hit All (Sword)",false,HitAll)
 
 task.spawn(function()
 	while wait()do -- because throttling is awesome
