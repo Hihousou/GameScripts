4c93fa9ce5d8c941c5212fc8e7287a0d94216096
Hihousou <108065780+Hihousou@users.noreply.github.com>
2022-09-16 18:12:52 +1000

    Update guestquestreforged.lua

--- guestquestreforged.lua
+++ guestquestreforged.lua
@@ -1,6 +1,6 @@
-_G.Active = true
-
+local AutoAttack = false
 local lPlayer = game.Players.LocalPlayer
+
 function get()
     if lPlayer.Character then
         local Humanoid = lPlayer.Character:FindFirstChild("Humanoid")
@@ -15,12 +15,12 @@ end
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hihousou/Hexploit/main/uilib.lua"))()
 local Window = Library:Window("Guest Quest Reforged")
 Window:Toggle("Auto Attack (Bow)",false,function(Bool)
-	_G.Active = Bool
+	AutoAttack = Bool
 end)
 
 task.spawn(function()
 	while wait()do -- because throttling is awesome
-		if _G.Active then
+		if AutoAttack then
           get()
         end
 	end
